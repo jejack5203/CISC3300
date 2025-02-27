d655c86c59fcd6f9cc5ff5fd7512430cbbd0abd2
Jodie <jjackson100@fordham.edu>
2025-02-27 17:25:19 -0500

    php-ic12

---
+++ php-ic12/index.php
@@ -0,0 +1,36 @@
+<?php
+//we need this to accept requests from any domain
+header("Access-Control-Allow-Origin: *");
+
+$uri = strtok($_SERVER["REQUEST_URI"], '?');
+
+//get uri pieces
+$uriArray = explode("/", $uri);
+//var_dump($uriArray);
+//0
+//1 forms
+
+
+if ($uriArray[1] === 'button' && $_SERVER['REQUEST_METHOD'] === 'GET') {
+    include 'in-class-12.html';} else {
+    $products = [
+    [   
+        "name" => "Ice Cream Cake", "price" => 19.99
+    ],
+
+    [
+        "name" => "Brownie", "price" => 2.99
+    ],
+    [
+        "name" => "Cakepop", "price" => 4.99
+    ],
+    [
+        "name" => "Cookie", "price" => 2.99
+    ]
+];
+
+echo json_encode($products);
+exit();
+} 
+
+?>
